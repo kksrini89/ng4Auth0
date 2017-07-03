eacaadeed881345fc28864a1d8fde5ba75414f1a
Srinivasan <kksrini89@gmail.com>
2017-07-03 22:21:43 +0530

    Removed Client Secret and Domain Name for Auth0

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -5,11 +5,9 @@ declare var Auth0Lock: any;
 
 @Injectable()
 export class Auth {
-    lock = new Auth0Lock("1t0l33OA3gLjZ250l75vNCFW6U26R0LL", "kkspizza.eu.auth0.com", {});
+    lock = new Auth0Lock("Client_Secret", "Domain_Name", {});
     constructor() {
-        this.lock.on("authenticated", authResult => {
-            // console.log(authResult);
-            // localStorage.setItem("id_token", authResult.idToken);
+        this.lock.on("authenticated", authResult => {            
             this.lock.getProfile(authResult.idToken, function (error: any, profile: any) {
                 if (error) {
                     throw new Error(error);
